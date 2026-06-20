bb0d8396cc4144f7fcccf1edc377baf428500d34
Sushmitha Hebbar <sushmitham579@gmail.com>
2026-06-20 11:06:50 +0530

    added the margin

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -5,8 +5,8 @@ const NotFound = () => {
     <div className="not-found-page-wrapper">
       <div className="not-found-container">
         <h1 className="not-found-code">404</h1>
-        <p className="not-found-message">Page not found</p>
-        <Link to="/" className="back-to-dashboard-link">
+        <p className="not-found-message">404 - Page Not Found</p>
+        <Link to="/" className="back-to-dashboard-link btn-try" style={{ marginTop: '0.5rem' }}>
           Back to dashboard
         </Link>
       </div>
